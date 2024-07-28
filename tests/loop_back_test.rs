d1f90a1ced2e1fcc64cbc1f7a98f4cc746c1f94d
Praveen Perera <me@praveenperera.com>
2024-07-28 11:26:02 -0500

    Add loop back test for test vector

--- tests/loop_back_test.rs
+++ tests/loop_back_test.rs
@@ -13,3 +13,15 @@ fn test_loop_back() {
     assert_eq!(labels_1, labels_2);
     assert_eq!(export_json.trim(), file_json.trim());
 }
+
+#[test]
+fn loop_back_test_vector() {
+    use pretty_assertions::assert_eq;
+
+    let labels_1 = Labels::try_from_file("tests/data/test_vector.jsonl").unwrap();
+    let export_json = labels_1.export().unwrap();
+
+    let labels_2 = Labels::try_from_str(&export_json).unwrap();
+
+    assert_eq!(labels_1, labels_2);
+}
